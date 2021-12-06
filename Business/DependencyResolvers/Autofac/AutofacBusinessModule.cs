c32fc370ba1741d67ee620b889fc8ce55bf09f2b
neslimanka <43746150+neslimanka@users.noreply.github.com>
2021-12-07 00:06:19 +0300

    Proje altyapısı oluşturuldu

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,57 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.Abstract;
+using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+using Core.Utilities.Security.JWT;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<BrandManager>().As<IBrandService>().SingleInstance();
+            builder.RegisterType<EfBrandDal>().As<IBrandDal>().SingleInstance();
+            Console.WriteLine("<--------------------------------------------------------->");
+            builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
+            builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+            Console.WriteLine("<--------------------------------------------------------->");
+            builder.RegisterType<ColorManager>().As<IColorService>().SingleInstance();
+            builder.RegisterType<EfColorDal>().As<IColorDal>().SingleInstance();
+            Console.WriteLine("<--------------------------------------------------------->");
+            builder.RegisterType<CustomerManager>().As<ICustomerService>().SingleInstance();
+            builder.RegisterType<EfCustomerDal>().As<ICustomerDal>().SingleInstance();
+            Console.WriteLine("<--------------------------------------------------------->");
+            builder.RegisterType<RentalManager>().As<IRentalService>().SingleInstance();
+            builder.RegisterType<EfRentalDal>().As<IRentalDal>().SingleInstance();
+            Console.WriteLine("<--------------------------------------------------------->");
+            builder.RegisterType<UserManager>().As<IUserService>().SingleInstance();
+            builder.RegisterType<EfUserDal>().As<IUserDal>().SingleInstance();
+            Console.WriteLine("<--------------------------------------------------------->");
+            builder.RegisterType<AuthManager>().As<IAuthService>().SingleInstance();
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>().SingleInstance();
+            Console.WriteLine("<--------------------------------------------------------->");
+            builder.RegisterType<ConsumerManager>().As<IConsumerService>().SingleInstance();
+            builder.RegisterType<EfConsumerDal>().As<IConsumerDal>().SingleInstance();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+
+
+
+        }
+    }
+}
---
+++ Core/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Core.Utilities.IoC;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        //IServiceCollection bizim uygulamamızın servis bağımlılıklarını eklediğiniz yada araya girmesini istediğimiz 
+        //koleksiyondur
+        public static IServiceCollection AddDependencyResolvers
+            (this IServiceCollection serviceCollection,ICoreModule[] modules)
+        {
+            foreach (var module in modules)
+            {
+                module.Load(serviceCollection);
+            }
+            return ServiceTool.Create(serviceCollection);
+            //Core katmanıda dahil olmak üzere ekleyeceğimiz tüm injectionları birarada toplayabileceğmiz yapıya dönüştü
+
+        }
+    }
+}
